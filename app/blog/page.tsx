bcc791e6b87c074c9f6e78e2d7a681d6fcb69f78
mitsunaga <mitsunaga@fieldweb.co.jp>
2024-01-29 13:29:38 +0900

    blog一覧ssr調整

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,16 +3,8 @@ import * as Blog from "@/features/blog/components/Index";
 import { newtClient } from "@/features/libs/newt";
 import React from "react";
 
-const BlogPage = async () => {
-  const { items: blog }: any = await newtClient.getContents({
-    appUid: "blog",
-    modelUid: "article",
-    query: {
-      select: ["_id", "title", "slug", "body", "coverImage", "tags"],
-      order: ["-_priority", "-_sys.customOrder"],
+const BlogPage = async ({ blog }: any) => {
 
-    },
-  });
 
 
   return (
@@ -22,5 +14,21 @@ const BlogPage = async () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const { items: blog } = await newtClient.getContents({
+    appUid: "blog",
+    modelUid: "article",
+    query: {
+      select: ["_id", "title", "slug", "body", "coverImage", "tags"],
+      order: ["-_priority", "-_sys.customOrder"],
+    },
+  });
+
+  return {
+    props: { blog }, // これにより、サーバー側で取得したデータがBlogPageコンポーネントに渡されます
+  };
+}
+
 
 export default BlogPage;
